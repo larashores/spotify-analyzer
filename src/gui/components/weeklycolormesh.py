2b287ecbcb78919049157a0148615651805bfc4d
Lara Shores <lara.shores@outlook.com>
2020-10-25 23:50:43 -0600

    Add color option for colormesh

--- src/gui/components/weeklycolormesh.py
+++ src/gui/components/weeklycolormesh.py
@@ -8,19 +8,17 @@ from matplotlib.colors import ListedColormap
 
 import utils
 from gui.components import PlotComponent
-from gui.options import ArtistChooser, Spinbox
+from gui.options import ArtistChooser, ColorMap, Spinbox
 from track import Track
-
-
-def _colormap(hue: float) -> ListedColormap:
-    return ListedColormap([colorsys.hsv_to_rgb(hue, saturation / 255, 1) for saturation in range(255)])
 
 
 class WeeklyColorMesh(PlotComponent):
     name = "Weekly Color Mesh"
-    adjust = plot.SubplotsAdjust(left=0.08, right=0.975, top=0.975, bottom=0.09)
+    adjust = plot.SubplotsAdjust(left=0.12, right=0.975, top=0.975, bottom=0.09)
 
-    def subplot(self, tracks: List[Track]) -> plot.SubPlot:  # type: ignore # pylint: disable=arguments-differ
+    options = (ColorMap,)
+
+    def subplot(self, tracks: List[Track], color_map: ListedColormap) -> plot.SubPlot:  # type: ignore # pylint: disable=arguments-differ
 
         values = [[0 for i in range(24)] for i in range(7)]
         for track in tracks:
@@ -28,7 +26,7 @@ class WeeklyColorMesh(PlotComponent):
             values[-((utils.in_day(track).weekday() - 5)) % 7][utils.in_hour(track).hour - 1] += 1
 
         return plot.SubPlot(
-            plot.Colormesh(values, _colormap(240 / 360)),
+            plot.Colormesh(values, color_map),
             x_tick_options=plot.TickOptions(
                 labels=[f"{i+1}\nam" for i in range(11)] + ["12\npm"] + [f"{i+1}\npm" for i in range(11)] + ["12\nam"],
                 values=[i + 0.5 for i in range(24)],
--- src/gui/options.py
+++ src/gui/options.py
@@ -1,11 +1,18 @@
+import colorsys
 import tkinter as tk
 from tkinter import ttk
-from typing import Any, List, Protocol, Set
+from typing import Any, Callable, List, Optional, Protocol, Set
+
+from matplotlib.colors import ListedColormap
 
 import utils
 from gui.searchablecombobox import SearchableComboBox
 from track import Track
 from type_hints import Parent
+
+
+def _hue_colormap(hue: float) -> ListedColormap:
+    return ListedColormap([colorsys.hsv_to_rgb(hue, saturation / 255, 1) for saturation in range(255)])
 
 
 class OptionWidget(ttk.Frame):
@@ -144,3 +151,97 @@ class ArtistChooser(OptionWidget):
 
     def _configure_combo_box(self) -> None:
         self._combo_box.config(values={artist for artist in self._artists if artist not in self.get_value()})
+
+
+class HueChooser(ttk.Frame):
+    def __init__(
+        self, parent: Parent = None, *, samples: int = 5, hue: float = 0.0, command: Optional[Callable[[], None]] = None
+    ):
+        super().__init__(parent)
+        scale_frame = ttk.Frame(self)
+        label = ttk.Label(scale_frame, text="Color")
+        scale = ttk.Scale(scale_frame, command=self._on_scale_move, value=hue)
+        frame_frame = ttk.Frame(self)
+        self._frames = [tk.Frame(frame_frame, width=50, height=50) for color in range(samples)]
+        self._accept_button = ttk.Button(self, text="Okay", command=command)
+
+        scale_frame.pack(expand=True, fill=tk.X)
+        label.pack(side=tk.LEFT)
+        scale.pack(side=tk.LEFT, expand=True, fill=tk.X)
+
+        frame_frame.pack(expand=True, fill=tk.BOTH)
+        for frame in self._frames:
+            frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+
+        self._accept_button.pack()
+        self._set_hue(hue)
+
+    def config(self, **kwargs) -> None:
+        if "command" in kwargs:
+            command = kwargs.pop("command")
+            self._accept_button.config(command=command)
+        super().config(**kwargs)
+
+    @property
+    def hue(self) -> float:
+        return self._hue
+
+    def _on_scale_move(self, hue: str) -> None:
+        self._set_hue(float(hue))
+
+    def _set_hue(self, hue: float) -> None:
+        self._hue = hue
+        self._color_map = _hue_colormap(hue)
+        for ind, frame in enumerate(self._frames):
+            color = [round(i * 255) for i in self._color_map.colors[ind * self._color_map.N // len(self._frames)]]
+            frame.config(background=f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}")
+
+
+def ask_hue(parent: Parent = None, *, hue: float = 0.0) -> float:
+    toplevel = tk.Toplevel(parent)
+    chooser = HueChooser(toplevel, hue=hue)
+
+    class AcceptCommand:
+        def __init__(self):
+            self.hue = None
+
+        def __call__(self):
+            toplevel.destroy()
+            self.hue = chooser.hue
+
+    command = AcceptCommand()
+    toplevel.protocol("WM_DELETE_WINDOW", command)
+    chooser.config(command=command)
+    chooser.pack(expand=True, fill=tk.BOTH)
+    toplevel.wait_window()
+    return command.hue
+
+
+class ColorMap(OptionWidget):
+    def __init__(self, parent: Parent = None):
+        super().__init__(parent)
+
+        button = ttk.Label(self, text="Color: ")
+        self._color_frame = tk.Frame(self, relief="ridge")
+
+        button.pack(side=tk.LEFT)
+        self._color_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=(5, 0), pady=2)
+
+        self._color_frame.bind("<Button-1>", self._on_click)
+
+        self._hue = 240 / 360
+        self._color_map = _hue_colormap(self._hue)
+        self._set_color()
+
+    def get_value(self) -> ListedColormap:
+        return self._color_map
+
+    def _on_click(self, event: tk.Event) -> None:
+        if (hue := ask_hue(hue=self._hue)) is not None:
+            self._hue = hue
+            self._color_map = _hue_colormap(self._hue)
+            self._set_color()
+
+    def _set_color(self) -> None:
+        color = [round(i * 255) for i in self._color_map.colors[-1]]
+        self._color_frame.config(background=f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}")
